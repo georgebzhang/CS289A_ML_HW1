a3f20b1db4f3e11b28d2a6800e159f2b52520c09
georgebzhang <43728180+georgebzhang@users.noreply.github.com>
2019-02-19 11:59:55 -0800

    read data

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print("hi")
-print("test")
-print("test2")
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 not detected.")
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+from sklearn import svm
+from scipy import io
+
+np.random.seed(69)
+
+for data_name in ["mnist", "spam", "cifar10"]:
+    data = io.loadmat("data/%s_data.mat" % data_name)
+    print("\nloaded %s data!" % data_name)
+    fields = "test_data", "training_data", "training_labels"
+    for field in fields:
+        print(field, data[field].shape)
